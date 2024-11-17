c21cb58a8e92b56077d5ee0b11a59d5179d19482
rifahnew6@gmail.com <rifahnew6@gmail.com>
2024-11-16 15:33:58 -1200

    Penambahan Tugas 2

---
+++ pemanggilan_variable_2.java
@@ -0,0 +1,9 @@
+public class pemanggilan_variable_2 {
+    public static void main(String[] args) {
+        String firstname = "Rifah";
+        String lastname = "Masrifah";
+
+        System.out.println("Nama Depan: " + firstname);
+        System.out.println("Nama Belakang: " + lastname);
+    }
+}
